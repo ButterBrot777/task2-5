76852c17b5c6b936246861f74921eee02b4f786d
Pavel Karenda <korendos@gmail.com>
2020-10-12 22:08:34 +0300

    feat: add tasks in the project

---
+++ src/resources/tasks/task.service.js
@@ -0,0 +1,13 @@
+const tasksRepo = require('./task.memory.repository');
+
+const getAll = () => tasksRepo.getAll();
+
+const getByID = (id) => tasksRepo.getByID(id);
+
+const create = (task) => tasksRepo.create(task);
+
+const update = (id, body) => tasksRepo.update(id, body);
+
+const remove = (id) => tasksRepo.remove(id);
+
+module.exports = { getAll, getByID, create, update, remove };
