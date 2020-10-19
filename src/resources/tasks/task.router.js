a04b53ed0206c65dfa95d2c2de5282fe82ae9d0a
Pavel Karenda <korendos@gmail.com>
2020-10-19 23:21:25 +0300

    fix: add fn errorHandler wrapper

--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -1,42 +1,95 @@
 const router = require('express').Router();
 const tasksService = require('./task.service');
 const Task = require('./task.model');
+const { catchErrors } = require('../../common/errorHandler');
 
-router.route('/').get(async (req, res) => {
-  const tasks = await tasksService.getAll();
-  res.json(tasks);
-});
+// router.route('/').get(async (req, res) => {
+//   const tasks = await tasksService.getAll();
+//   res.json(tasks);
+// });
+router.route('/:boardId/tasks').get(
+  catchErrors(async (req, res) => {
+    const tasks = await tasksService.getAll(req.params.boardId);
+    await res.status(200).json(tasks);
+  })
+);
 
-router.route('/').post(async (req, res) => {
-  const task = await tasksService.create(new Task(req.body));
-  res.json(task);
-});
-
-router.route('/:id').get(async (req, res) => {
-  try {
-    const user = await tasksService.getByID(req.params.id);
-    res.json(user);
-  } catch (error) {
-    res.status(404).send(error.message);
-  }
-});
-
-router.route('/:id').put(async (req, res) => {
-  try {
-    const task = await tasksService.update(req.params.id, req.body);
+// router.route('/').post(async (req, res) => {
+//   const task = await tasksService.create(new Task(req.body));
+//   res.json(task);
+// });
+router.route('/:boardId/tasks').post(
+  catchErrors(async (req, res) => {
+    const task = await tasksService.create(new Task(req.body));
     res.json(task);
-  } catch (error) {
-    res.status(404).send(error.message);
-  }
-});
+  })
+);
 
-router.route('/:id').delete(async (req, res) => {
-  try {
-    const task = await tasksService.remove(req.params.id);
-    res.json(task);
-  } catch (error) {
-    res.status(404).send(error.message);
-  }
-});
+// router.route('/:id').get(async (req, res) => {
+//   try {
+//     const user = await tasksService.getByID(req.params.id);
+//     res.json(user);
+//   } catch (error) {
+//     res.status(404).send(error.message);
+//   }
+// });
+router.route('/:id').get(
+  catchErrors(async (req, res) => {
+    const task = await tasksService.getByID(req.params.id);
+    if (task) {
+      await res.status(200).json();
+    } else {
+      const error = new Error();
+      error.status = 404;
+      throw error;
+    }
+  })
+);
+
+// router.route('/:id').put(async (req, res) => {
+//   try {
+//     const task = await tasksService.update(req.params.id, req.body);
+//     res.json(task);
+//   } catch (error) {
+//     res.status(404).send(error.message);
+//   }
+// });
+router.route('/:id').put(
+  catchErrors(async (req, res) => {
+    const task = await tasksService.updateTask(
+      req.params.boardId,
+      req.params.taskId,
+      req.body
+    );
+    if (task) {
+      await res.json(Task.toResponse(task));
+    } else {
+      const error = new Error();
+      error.status = 404;
+      throw error;
+    }
+  })
+);
+
+// router.route('/:id').delete(async (req, res) => {
+//   try {
+//     const task = await tasksService.remove(req.params.id);
+//     res.json(task);
+//   } catch (error) {
+//     res.status(404).send(error.message);
+//   }
+// });
+router.route('/:id').delete(
+  catchErrors(async (req, res) => {
+    if (await tasksService.remove(req.params.id)) {
+      const task = await tasksService.remove(req.params.id);
+      res.status(204).json(task);
+    } else {
+      const error = new Error();
+      error.status = 404;
+      throw error;
+    }
+  })
+);
 
 module.exports = router;
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,43 +1,93 @@
 const router = require('express').Router();
 const User = require('./user.model');
 const usersService = require('./user.service');
+const { catchErrors } = require('../../common/errorHandler');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+// router.route('/').get(async (req, res) => {
+//   const users = await usersService.getAll();
+//   // map user fields to exclude secret fields like "password"
+//   res.json(users.map(User.toResponse));
+// });
+router.route('/').get(
+  // wrap function into errorHandler callback
+  catchErrors(async (req, res) => {
+    const users = await usersService.getAll();
+    // map user fields to exclude secret fields like "password"
+    res.json(users.map(User.toResponse));
+  })
+);
 
-router.route('/').post(async (req, res) => {
-  const user = await usersService.create(new User(req.body));
-  res.json(User.toResponse(user));
-});
+// router.route('/').post(async (req, res) => {
+//   const user = await usersService.create(new User(req.body));
+//   res.json(User.toResponse(user));
+// });
+router.route('/').post(
+  catchErrors(async (req, res) => {
+    const user = await usersService.create(new User(req.body));
+    res.status(200).json(User.toResponse(user));
+  })
+);
 
-router.route('/:id').get(async (req, res) => {
-  try {
+// router.route('/:id').get(async (req, res) => {
+//   try {
+//     const user = await usersService.getByID(req.params.id);
+//     res.json(User.toResponse(user));
+//   } catch (error) {
+//     res.status(404).send(error.message);
+//   }
+// });
+router.route('/:id').get(
+  catchErrors(async (req, res) => {
     const user = await usersService.getByID(req.params.id);
-    res.json(User.toResponse(user));
-  } catch (error) {
-    res.status(404).send(error.message);
-  }
-});
+    if (user) {
+      res.status(200).json(User.toResponse(user));
+    } else {
+      const error = new Error();
+      error.status = 404;
+      throw error;
+    }
+  })
+);
 
-router.route('/:id').put(async (req, res) => {
-  try {
+// router.route('/:id').put(async (req, res) => {
+//   try {
+//     const user = await usersService.update(req.params.id, req.body);
+//     res.json(User.toResponse(user));
+//   } catch (error) {
+//     res.status(404).send(error.message);
+//   }
+// });
+router.route('/:id').put(
+  catchErrors(async (req, res) => {
     const user = await usersService.update(req.params.id, req.body);
-    res.json(User.toResponse(user));
-  } catch (error) {
-    res.status(404).send(error.message);
-  }
-});
+    if (user) {
+      await res.status(200).json(User.toResponse(user));
+    } else {
+      const error = new Error();
+      error.status = 401;
+      throw error;
+    }
+  })
+);
 
-router.route('/:id').delete(async (req, res) => {
-  try {
-    const deleted = await usersService.remove(req.params.id);
-    res.json(User.toResponse(deleted));
-  } catch (error) {
-    res.status(404).send(error.message);
-  }
-});
+// router.route('/:id').delete(async (req, res) => {
+//   try {
+//     const deleted = await usersService.remove(req.params.id);
+//     res.json(User.toResponse(deleted));
+//   } catch (error) {
+//     res.status(404).send(error.message);
+//   }
+// });
+router.route('/:id').delete(
+  catchErrors(async (req, res) => {
+    if (await usersService.remove(req.params.id)) {
+      res.status(204).json();
+    } else {
+      const error = new Error();
+      error.status = 401;
+      throw error;
+    }
+  })
+);
 
 module.exports = router;
